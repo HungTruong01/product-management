6ef8275b9f6e3fdbe55590bcd3d3df341bfa6952
Hung Truong <hung85405@gmail.com>
2023-12-24 10:50:49 +0700

    Connect database

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ require("dotenv").config();
 
 const route = require("./routes/client/index.route");
 
+const database = require("./config/database");
+
 const app = express();
 const port = process.env.PORT;
 
+database.connect();
+
 app.set("views", "./views");
 app.set("view engine", "pug");
+
+app.use(express.static("public"));
 
 route(app);
 

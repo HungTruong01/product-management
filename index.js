7b335ab2c86ed2dad01b3d4963e4e718a7e9a460
Hung Truong <hung85405@gmail.com>
2023-12-25 15:31:21 +0700

    Set up layout

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 require("dotenv").config();
 
+const routeAdmin = require("./routes/admin/index.route");
 const route = require("./routes/client/index.route");
+
+const systemConfig = require("./config/system")
 
 const database = require("./config/database");
 
@@ -15,6 +18,10 @@ app.set("view engine", "pug");
 
 app.use(express.static("public"));
 
+// App Local Variable
+app.locals.prefixAdmin = systemConfig.prefixAdmin
+
+routeAdmin(app);
 route(app);
 
 app.listen(port, () => {

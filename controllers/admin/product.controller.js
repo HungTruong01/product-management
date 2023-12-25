d8003da69f9d909d8f2c562f4953faed8d01ad00
Hung Truong <hung85405@gmail.com>
2023-12-25 22:00:03 +0700

    Lay data in ra giao dien san pham

---
+++ controllers/admin/product.controller.js
@@ -0,0 +1,16 @@
+// [GET] /admin/product
+
+const Product = require("../../models/product.model");
+
+module.exports.index = async (req,res) => {
+    const products = await Product.find({
+        deleted: false
+    });
+
+    console.log(products);
+
+    res.render("admin/pages/products/index", {
+        pageTitle: "Trang danh sách sản phẩm",
+        products: products
+    });
+};

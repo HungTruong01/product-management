d0b1519eb100a639d062c219c900d9ae3c06d193
Hung Truong <hung85405@gmail.com>
2023-12-27 21:29:07 +0700

    Toi uu bo loc

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,26 +1,10 @@
 const Product = require("../../models/product.model");
+const filterStatusHelper = require("../../helpers/filterStatus");
 // [GET] /admin/product
 
 module.exports.index = async (req,res) => {
-    //console.log(req.query.status)
-    
-    let filterStatus = [
-        {
-            name: "Tất cả",
-            status: "",
-            class: ""
-        },
-        {
-            name: "Hoạt động",
-            status: "active",
-            class: ""
-        },
-        {
-            name: "Dừng hoạt động",
-            status: "inactive",
-            class: ""
-        }
-    ];
+    // lọc trạng thái
+    const  filterStatus = filterStatusHelper(req.query);
 
     let find = {
         deleted: false
@@ -38,14 +22,7 @@ module.exports.index = async (req,res) => {
         find.title = regex;
     }
 
-    if(req.query.status) {
-        const index = filterStatus.findIndex(item => item.status == req.query.status);
-        filterStatus[index].class = "active";
-    }
-    else {
-        const index = filterStatus.findIndex(item => item.status == "");
-        filterStatus[index].class = "active"
-    }
+    
 
     const products = await Product.find(find);
 
